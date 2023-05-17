bf33dba8419c26dd19b2bcfe3be5f2b786c3d24a
marcs-feh <marcosfehlauer@protonmail.com>
2023-05-17 13:37:23 -0300

    create Image component, fetch board image from pb

--- front-end/src/routes/+page.svelte
+++ front-end/src/routes/+page.svelte
@@ -1,46 +1,54 @@
 <script lang="ts">
-	import { json } from "@sveltejs/kit";
-import PocketBase, { Record } from "pocketbase";
-import { onMount } from "svelte";
+	import PocketBase, { Record } from "pocketbase";
+	import { onMount } from "svelte";
+	import { PB_URL } from '../stores';
+	import Image from "../lib/Image.svelte";
 
-let pb = new PocketBase('http://127.0.0.1:8090')
+	let pb = new PocketBase('http://127.0.0.1:8090')
 
-let userList: Record[] = []
+	let userList: Record[] = []
+	let boardList: Record[] = []
 
-const updateUserList = async () => {
-  let users = await pb.collection('users').getFullList({})
-  userList = users
-  console.table(userList)
-}
+	// const updateUserList = async () => {
+	// 	let users = await pb.collection('users').getFullList({})
+	// 	userList = users
+	// 	console.table(userList)
+	// }
 
-onMount(() => {
-  updateUserList()
-})
+	const updateBoardList = async () => {
+		boardList = await pb.collection('boards').getFullList()
+		console.table(boardList)
+	}
 
-let size = 1
-let benis = '8D'
+	const composeImageURL = (name : string, collectionIdOrName : string, recordId : string) => {
+		// http://127.0.0.1:8090/api/files/COLLECTION_ID_OR_NAME/RECORD_ID/FILENAME
+		// http://localhost:8090/api/files/8nzz6c4vb2w7xtt/wfrnnv2xxud2dp9/fumo_4MRbYujrfR.png?thumb=100x100
+		let src = `${PB_URL}/api/files/${collectionIdOrName}/${recordId}/${name}`
+		console.log(`src: ${src}`)
+		return src
+	}
 
-const updateBenis = (n:number) => {
-  benis = '8'
-  for(let i = 0; i < n; i++){
-    benis += '='
-  }
-  benis += 'D'
-}
+	onMount(() => {
+		updateBoardList()
+	})
 
 </script>
 
 <main>
-<div>
-{ benis }
-</div>
-<button on:click={ () => {size += 1; updateBenis(size); }  }> Rub :DDD</button>
-
-<h2>Problematicos cadastrados:</h2>
-{#each userList as user}
-<li>
-  { user['name'] }
-</li>
-{/each}
-
+	<h1>/J</h1>
+	<h2>Boards:</h2>
+	<div class="boardlist">
+		<ul>
+			{#each boardList as board}
+				<li><span class="boardtitle">{board.name}</span>: {board.description}</li>
+				<Image src={composeImageURL(board.banner, board.collectionName, board.id)} />
+			{/each}
+		</ul>
+	</div>
 </main>
+
+<style>
+	.boardtitle {
+		font-family: monospace;
+	}
+</style>
---
+++ front-end/src/stores.ts
@@ -0,0 +1,3 @@
+// import { } from 'svelte/store'
+
+export const PB_URL = 'http://localhost:8090'
